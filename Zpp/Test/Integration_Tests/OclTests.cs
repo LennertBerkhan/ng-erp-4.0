c8454f0e365c22a59e557f633963616757560c23
yuri <yuri@encnet.org>
2019-12-06 19:30:55 +0100

    redirect Console to TestOutput

--- Zpp/Test/Integration_Tests/OclTests.cs
+++ Zpp/Test/Integration_Tests/OclTests.cs
@@ -1,6 +1,10 @@
+using System;
+using System.IO;
 using System.Reflection;
+using System.Text;
 using OclAspectTest;
 using Xunit;
+using Xunit.Abstractions;
 using Zpp.DataLayer;
 using Zpp.DataLayer.impl.ProviderDomain.Wrappers;
 using Zpp.ZppSimulator;
@@ -9,16 +13,21 @@ namespace Zpp.Test.Integration_Tests
 {
     public class OclTests : AbstractTest
     {
+        public OclTests(ITestOutputHelper output) : base(true)
+        {
+            Console.SetOut(new Converter(output));
+        }
+
         [Fact]
         public void OCL_ProductionOrderOperationScheduling()
         {
             var ocls = @"
-context ProductionOrderOperation::GetValue()
-    post ProductionOrderOperationScheduling:
-        self._productionOrderOperation.StartBackward >= 0 
-            or self._productionOrderOperation.StartForward >= 0 
-                and self._productionOrderOperation.EndForward >= 0
-";
+        context ProductionOrderOperation::GetValue()
+            post ProductionOrderOperationScheduling:
+                self._productionOrderOperation.StartBackward >= 0 
+                    or self._productionOrderOperation.StartForward >= 0 
+                        and self._productionOrderOperation.EndForward >= 0
+        ";
             OclTestProvider.AddConstraints(new[] {"Zpp", "Master40.DB"}, ocls);
 
             var zppSimulator = new ZppSimulator.impl.ZppSimulator();
@@ -30,6 +39,29 @@ context ProductionOrderOperation::GetValue()
                 .ProductionOrderOperationGetAll())
             {
                 var v = productionOrderOperation.GetValue();
+                Assert.True(!(v is null));
+            }
+        }
+
+        private class Converter : TextWriter
+        {
+            ITestOutputHelper _output;
+
+            public Converter(ITestOutputHelper output)
+            {
+                _output = output;
+            }
+
+            public override Encoding Encoding => Encoding.Default;
+
+            public override void WriteLine(string message)
+            {
+                _output.WriteLine(message);
+            }
+
+            public override void WriteLine(string format, params object[] args)
+            {
+                _output.WriteLine(format, args);
             }
         }
     }
